5598f80f7dcabdbff134416449803aadb0b0a1a3
kreazzyprogramer <kreazzyprogramer@gmail.com>
2021-11-23 21:31:23 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import SignIn from "./components/Sign-In";
 import SignUp from "./components/Sign-Up";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SignIn1 from "./components/Sign-In-1";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/"  element={<SignIn />} />
         <Route path="/SignUp"  element={<SignUp />} />
+        <Route path="/Signin-1"  element={<SignIn1 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Sign-In.js
+++ src/components/Sign-In.js
@@ -1,28 +1,33 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link as RouterLink} from 'react-router-dom';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link as RouterLink } from "react-router-dom";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       {/* <Link color="inherit" href="#">
         KreazZy-Programer
       </Link>{' '} */}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -35,8 +40,8 @@ export default function SignIn() {
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -47,18 +52,25 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main', width: 70, height: 70   }}>
+          <Avatar
+            sx={{ m: 1, bgcolor: "secondary.main", width: 70, height: 70 }}
+          >
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -91,22 +103,26 @@ export default function SignIn() {
             >
               Sign In
             </Button>
-            <Grid container>
+            <Grid container spacing={2}>
               <Grid item xs>
-                <Link variant="body2">
-                  Forgot password?
+                <Link variant="body2">Forgot password?</Link>
+              </Grid>
+              <Grid item >
+                <Link component={RouterLink} to="/SignUp" variant="body2">
+                  Don't have an account? Sign Up
                 </Link>
               </Grid>
               <Grid item>
-                <Link component={RouterLink} to="/SignUp" variant="body2">
-                  Don't have an account? Sign Up
+                <Link component={RouterLink} to="/Signin-1" variant="body2">
+                  
+                  Different SignIn Page
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
         <Copyright sx={{ mt: 10, mb: 2 }} />
-      </Container>
+          </Container>
     </ThemeProvider>
   );
 }
